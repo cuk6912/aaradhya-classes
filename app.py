e8e203567f46b46dfe934010959b43a8c59fb10c
cuk6912 <cuk6912@gmail.com>
2026-03-04 16:05:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request, redirect
+from models import db, Student
 
 app = Flask(__name__)
 
@@ -11,6 +11,28 @@ db.init_app(app)
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route("/students")
+def students():
+    students = Student.query.all()
+    return render_template("students.html", students=students)
+
+@app.route("/add_student", methods=["POST"])
+def add_student():
+    name = request.form["name"]
+    student_class = request.form["class"]
+    phone = request.form["phone"]
+
+    student = Student(
+        name=name,
+        student_class=student_class,
+        phone=phone
+    )
+
+    db.session.add(student)
+    db.session.commit()
+
+    return redirect("/students")
 
 with app.app_context():
     db.create_all()
