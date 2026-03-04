fa9a012e70d5d5e7b0958910598fce95ce66b1d6
cuk6912 <cuk6912@gmail.com>
2026-03-04 16:31:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import os
 from flask import Flask, render_template, request, redirect
-from models import db, Student
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Railway PostgreSQL connection
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# PostgreSQL connection from Railway
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+
+
+class Student(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100))
+
+    student_class = db.Column(db.String(20))
+
+    subject = db.Column(db.String(50))
+
+    phone = db.Column(db.String(20))
+
+    monthly_fee = db.Column(db.Integer)
+
 
 @app.route("/")
 def dashboard():
@@ -28,20 +44,30 @@ def students():
 def add_student():
 
     name = request.form["name"]
+
     student_class = request.form["class"]
+
     subject = request.form["subject"]
+
     phone = request.form["phone"]
+
     fee = request.form["fee"]
 
-    student = Student(
+    new_student = Student(
+
         name=name,
+
         student_class=student_class,
+
         subject=subject,
+
         phone=phone,
+
         monthly_fee=fee
     )
 
-    db.session.add(student)
+    db.session.add(new_student)
+
     db.session.commit()
 
     return redirect("/students")
@@ -52,4 +78,5 @@ with app.app_context():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
