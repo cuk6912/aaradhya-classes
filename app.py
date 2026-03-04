fac5f11bf50899d12c4ccd843df8dae298f0f1b9
cuk6912 <cuk6912@gmail.com>
2026-03-04 16:00:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
