9c5ad3be701d8492c85c31b8d7a80d294cecb37b
cuk6912 <cuk6912@gmail.com>
2026-03-04 16:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask, render_template, request, redirect
 from models import db, Student
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+# Railway PostgreSQL connection
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
@@ -11,6 +14,7 @@ db.init_app(app)
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
+
 
 @app.route("/students")
 def students():
@@ -24,37 +28,28 @@ def students():
 def add_student():
 
     name = request.form["name"]
-
     student_class = request.form["class"]
-
     subject = request.form["subject"]
-
     phone = request.form["phone"]
-
     fee = request.form["fee"]
 
     student = Student(
-
         name=name,
-
         student_class=student_class,
-
         subject=subject,
-
         phone=phone,
-
         monthly_fee=fee
-
     )
 
     db.session.add(student)
-
     db.session.commit()
 
     return redirect("/students")
 
+
 with app.app_context():
     db.create_all()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
