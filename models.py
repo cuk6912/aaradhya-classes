54979c60b85952da4d0297f99fa6285053e01f79
cuk6912 <cuk6912@gmail.com>
2026-03-04 16:04:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,11 +5,5 @@ db = SQLAlchemy()
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    class_name = db.Column(db.String(50))
-    phone = db.Column(db.String(20))
-
-class Teacher(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    subject = db.Column(db.String(50))
+    student_class = db.Column(db.String(50))
     phone = db.Column(db.String(20))
