3190178fb56af07e2a13269a303776b526feba86
cuk6912 <cuk6912@gmail.com>
2026-03-05 12:15:44 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,31 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model):
-
+class Teacher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    subject = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
 
-    username = db.Column(db.String(50), unique=True)
 
-    password = db.Column(db.String(100))
-
-    role = db.Column(db.String(20))
+class Batch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    batch_name = db.Column(db.String(100))
+    class_name = db.Column(db.String(20))
+    subject = db.Column(db.String(100))
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))
+    time = db.Column(db.String(50))
 
 
 class Student(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
 
-    name = db.Column(db.String(100), nullable=False)
-
+    name = db.Column(db.String(100))
     student_class = db.Column(db.String(20))
-
     subject = db.Column(db.String(100))
-
     phone = db.Column(db.String(20))
 
     monthly_fee = db.Column(db.Integer)
 
-    join_date = db.Column(db.String(20))
+    batch_id = db.Column(db.Integer, db.ForeignKey('batch.id'))
 
-    leave_date = db.Column(db.String(20))
+    join_date = db.Column(db.Date)
+    leave_date = db.Column(db.Date)
